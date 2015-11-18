f75b914d1542e2ebc53fa388ede7dd63fb251667
Helen <helen@puppetlabs.com>
2015-11-18 19:58:52 +0000

    -exec

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -8,9 +8,5 @@ class aliases (
     group => 'root',
     mode => '0644',
     content => template('aliases/aliases.erb'),
-}
-  exec { '/usr/bin/newaliases':
-    refreshonly => true,
-    subscribe => File['/etc/aliases'],
   }
 }
