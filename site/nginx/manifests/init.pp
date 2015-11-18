7f0725641cca1354de8c07fe90a0892b7f764321
Helen <helen@puppetlabs.com>
2015-11-18 14:29:24 +0000

    Ex12.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,28 +6,38 @@ class nginx {
     ensure => directory,
     owner => 'root',
     group => 'root',
-    mode => '0755',
+    mode => '0775',
   }
-  file { '/var/www/index.html':
+    file { '/var/www/index.html':
     ensure => file,
     owner => 'root',
     group => 'root',
-    mode => '0644',
+    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
   }
   file { '/etc/nginx/conf.d':
     ensure => directory,
     owner => 'root',
     group => 'root',
-    mode => '0755',
+    mode => '0775',
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
   }
   service { 'nginx':
     ensure => running,
